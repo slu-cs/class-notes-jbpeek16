71ab0122bc86c01846e5d5c3019d9e3e4d2c4281
jbpeek <jbpeek16@stlawu.edu>
2020-01-29 14:21:46 -0500

    Problems

--- javascript/problems.js
+++ javascript/problems.js
@@ -15,3 +15,18 @@ let fib = (n) => {
     return (fib(n-1) + fib(n-2));
   }
 }
+
+console.log(fib(100));
+
+let count = 0;
+for (let x = 1; x < 101; x++) {
+  if (x%3 === 0) {
+    count += 1;
+  }
+  if (x%5 === 0) {
+    count += 1;
+  }
+  if ((x%3 === 0) && (x%5 === 0)) {
+    count -= 1;
+  }
+}
