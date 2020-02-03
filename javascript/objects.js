5e1ea78853c7df9a222279a48298f245fb50f02b
jbpeek <jbpeek16@stlawu.edu>
2020-02-03 13:23:07 -0500

    Functions

--- javascript/objects.js
+++ javascript/objects.js
@@ -10,7 +10,7 @@ console.log(point);
 const circle = {
   center: point,
   radius: 4
-}
+};
 
 console.log(circle.center.x);
 console.log(circle.color);
@@ -21,7 +21,7 @@ const languages = ['HTML', 'CSS', 'JS'];
 //accessing length and elements
 for (let i = 0; i < languages.length; i++) {
   console.log(languages[i]);
-}
+};
 
 // changing elements
 languages[2] = 'Javascript';
@@ -32,4 +32,14 @@ languages.push('MongoDB');
 // iterate over elements
 for (const lang of languages) {
   console.log(lang);
-}
+};
+
+const avg = function(a, b) {
+  return (a + b) / 2;
+};
+
+const printAB = function(a, b) {
+  console.log(`a = ${a}, b = ${b}`);
+};
+
+print(7, 8);
