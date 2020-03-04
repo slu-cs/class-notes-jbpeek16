08e64719daa9719512a160b9f01e4648045e1f4a
jbpeek <jbpeek16@stlawu.edu>
2020-03-04 13:34:41 -0500

    Server

--- express/hello/app.js
+++ express/hello/app.js
@@ -6,5 +6,22 @@ app.get('/', function(request, response) {
   response.send('Hello Jonas');
 })
 
+app.get('/foo', function(request, response) {
+  response.send('Hello Foo');
+})
+
+app.get('/foo/bar', function(request, response) {
+  response.send('Hello Foo Bar');
+})
+
+app.get('/zap/:z', function(request, response) {
+  response.send(`Hello ${request.params.z}`);
+})
+
+app.get('/zap', function(request, response) {
+  response.send(`Hello zap ${request.params.x} and ${request.params.y}`);
+})
+
+
 app.listen(3000);
-console.log('Server is runnign');
+console.log('Server is running');
